3f4c79f587232c1c5ba31f9351fb026c53f17502
pa454420-blip <pa454420@gmail.com>
2026-09-06 20:47:42 +0800

    Menambahkan jumlah kendaraan

--- app/data.js
+++ app/data.js
@@ -5,7 +5,7 @@ const kendaraan = [
         jenis : "MPV",
         harga :40000000,
         status : "Tersedia",
-        gambar : "https://i.pinimg.com/736x/2c/7e/4c/2c7e4cf27fec36f144cd79a49509be09.jpg"
+        gambar : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSONt6cATdFQAp3Lf0QE3ShgaKN5pKY5W50GEVCvkEIUw&s=10"
     },
     {
         id : 2,
@@ -15,6 +15,22 @@ const kendaraan = [
         status : "tidak tersedia",
         gambar : "https://i.pinimg.com/736x/2c/7e/4c/2c7e4cf27fec36f144cd79a49509be09.jpg"
 
+    },
+    {
+        id : 3,
+        nama : "pajero",
+        jenis :"SUV"
+        harga :72400000,
+        status : "Tersedia",
+        gambar : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtKwPv1HKE71vVVcaPpwU7q2GJc2D4x7SYUvTYiiC4WTJwo8_zqOXWGQ-W&s=10"
+    },
+    {
+        id : 4,
+        nama : "Mitsubishi Xpander",
+        jenis : "MPV",
+        harga : 25000000,
+        status : "Tersedia",
+        gambar : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXqwb76ojOi5xjtGBVsToLRrBdrdGkrV3015sP0ciVpUKSETckE6sFW0nW&s=10"
     }
 ]
 export default kendaraan;
