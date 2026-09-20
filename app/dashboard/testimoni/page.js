226599c2d1c7138416ac64bed0d0fd7fbd853549
pa454420-blip <pa454420@gmail.com>
2026-09-20 14:00:11 +0800

    masuk dashboard

---
+++ app/dashboard/testimoni/page.js
@@ -0,0 +1,9 @@
+"use client";
+{
+  "nama": "Andi Pratama",
+  "rating": 5,
+  "pesan": "Pelayanan cepat, mobil bersih. Recommended!",
+  "foto": "https://.../uploads/testimonials/7.jpg",
+  "tampil": true,
+  "urutan": 1
+}
